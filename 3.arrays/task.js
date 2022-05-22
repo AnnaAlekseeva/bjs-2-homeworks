21b1f2c37ddd87c289663308df3e616cad724dc8
AnnaAlekseeva <annagordobaeva@yandex.ru>
2022-05-22 14:46:38 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,15 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
+  if (arr1.length === arr2.length){
+    result = arr1.every( (item)=>arr2[arr1.indexOf(item)] === item);
+  } else result = false;
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter((item)=>(item>0 && item % 3 === 0 )).map((item)=>item*10);
 
   return resultArr; // array
 }
