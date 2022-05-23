6f370cffed4f98132e94afa9f1e9888459364151
AnnaAlekseeva <annagordobaeva@yandex.ru>
2022-05-23 18:17:51 +0300

    Update task.js

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,41 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if(this.marks === undefined){ 
+    this.marks = [mark];
+   // this.marks.push(mark);
+    } else {
+    this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+  if(this.marks === undefined){ 
+    this.marks = [];
+    } 
+    for (let mark of marks) this.marks.push(mark);
+}
+
+Student.prototype.getAverage = function () {
+  let sum =  0;
+  if (this.marks.length === 0) {
+    return 0;
+} else{
+  for (let mark of this.marks) sum +=mark;
+  return sum/this.marks.length;
+}
+}
+
+Student.prototype.exclude = function (reason) {
+  this.excluded = reason;
+  delete(this.subject);
+  delete(this.marks);
+}
